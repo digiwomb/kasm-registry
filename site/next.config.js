2779919e9567b8130a046cac0600f05d7ce08262
digiwomb <digiwomb@outlook.de>
2024-12-23 10:56:31 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     name: 'Kasm Technologies',
     description: 'An unofficial store for Kasm supported workspaces.',
     icon: 'https://registry.kasmweb.com/1.1/_next/static/media/registry-logo.0c581e19.png',
-    listUrl: 'hhttps://digiwomb.github.io/kasm-registry/',
+    listUrl: 'https://digiwomb.github.io/kasm-registry/',
     contactUrl: 'https://github.com/digiwomb/kasm-registry/issues',
   },
   reactStrictMode: true,
